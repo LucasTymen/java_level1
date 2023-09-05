b1832527351438231bc09c5bdd64598cde3178aa
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-05 17:25:48 +0200

    review

---
+++ 01_intro/08_review/Review.java
@@ -0,0 +1,65 @@
+Java Review: Putting It All Together
+
+In this lesson, we’ve started writing our first programs in Java.
+
+We’ve also learned rules and guidelines for how to write Java programs:
+
+    Java programs have at least one class and one main() method.
+        Each class represents one real-world idea.
+        The main() method runs the tasks of the program.
+    Java comments add helpful context to human readers.
+    Java has whitespace, curly braces, and semicolons.
+        Whitespace is for humans to read code easily.
+        Curly braces mark the scope of a class and method.
+        Semicolons mark the end of a statement.
+    Java is a compiled language.
+        Compiling catches mistakes in our code.
+        Compilers transform code into an executable class.
+
+Instructions
+1.
+
+The text editor holds an empty file named Review.java. Fill it in!
+
+Define a public class named Review.
+
+Use opening and closing curly braces for the scope of the class.
+
+Remember, no semicolons for classes or methods!
+
+We define a class in Java like so:
+
+public class MyClass {
+  // class code goes here
+}
+
+2.
+
+This code produces an error because Java programs need a main() method.
+
+Define the main() method within the curly braces of the Review class.
+
+Every Java program has one main() method with the following signature:
+
+public static void main(String[] args) {
+  // method code goes here
+}
+
+3.
+
+Inside of the curly braces for the main() method, write The main method executes the tasks of the class as a single-line comment.
+
+We create a single-line comment like so:
+
+// Just a brief comment
+
+4.
+
+Below the comment, write a statement that prints the following: My first Java program from scratch!.
+
+We can print a statement with the following syntax:
+
+System.out.println("Blah blah blah");
+
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
