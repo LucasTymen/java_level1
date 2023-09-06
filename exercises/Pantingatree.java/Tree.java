37e7d522a5678cb328cefd25c3ca651c59b4050b
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-06 11:32:34 +0200

    planting tree exercise

---
+++ exercises/Pantingatree.java/Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args){
+    // statement
+    System.out.print("Hey there, I’m Cthuluh and I’m learning to code in Java like a god do!");
+    System.out.println("I’m going to plant a tree today! - Ready to get my hands dirty!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+}
+}
