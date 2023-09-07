7ae331f9acf3e8e5a425bbb2d811ab6f8dda9f03
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-07 17:19:42 +0200

    review

---
+++ 02_Java Program Structure/03_variables/09_review/MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {
+    String name = "Lulu Berlu";
+    int age = 52;
+    double desiredSalary = 99888.5;
+    char gender = 'm';
+    boolean lookingForJob = true;
+	}
+}
---
+++ 02_Java Program Structure/03_variables/09_review/Review.java
@@ -0,0 +1,61 @@
+/*
+Learn Java: Variables
+Review
+
+Creating and filling variables is a powerful concept that allows us to keep track of all kinds of data in our program.
+==> https://www.codecademy.com/resources/docs/java/variables
+
+In this lesson, we learned how to create and print several different data types in Java, which you’ll use as you create bigger and more complex programs.
+==> https://www.codecademy.com/resources/docs/java/data-types
+
+We covered:
+
+    int, which stores whole numbers.
+    double, which stores bigger whole numbers and decimal numbers.
+    boolean, which stores true and false.
+    char, which stores single characters using single quotes.
+    String, which stores multiple characters using double quotes.
+    Static typing, which is one of the safety features of Java.
+    Variable naming conventions.
+
+Practice declaring variables and assigning values to make sure you have a solid foundation for learning more complicated and exciting Java concepts!
+Instructions
+1.
+
+The file MyProfile.java contains a class that represents your hiring profile as presented to potential employers.
+
+In the main() method, create a variable called name that holds your name, as a sequence of characters.
+
+If we were defining a variable called userID that would hold a string of letters, we would use a line like this:
+*/
+String userID = "myUsername";
+/*
+You can use the same sort of declaration and initialization for your name variable!
+2.
+
+Create a variable called age that holds your age as a whole number.
+3.
+
+Create a variable called desiredSalary that holds your desired salary per year to a precision of two decimal points.
+
+If we were defining a variable called gpa that would hold a decimal number, we would use a line like this:
+*/
+double gpa = 5.1;
+/*
+You can use the same sort of declaration and initialization for your desiredSalary variable!
+4.
+
+Create a variable called gender that holds a single character, m (male), f (female), n (for none), or o (for other).
+5.
+
+Create a variable called lookingForJob that holds whether or not you are currently open to job offers.
+
+If we were defining a variable called isLightOn that would hold either true or false, we would use a line like this:
+*/
+boolean isLightOn = true;
+/*
+You can use the same sort of declaration and initialization for your lookingForJob variable!
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+==> https://www.codecademy.com/learn/learn-java/modules/learn-java-variables/cheatsheet
+*/
