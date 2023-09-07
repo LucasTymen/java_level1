45cd304a37788866d4ae620eb05ce12b3b4b24bd
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-07 16:57:55 +0200

    naming

---
+++ 02_Java Program Structure/03_variables/08_naming/BadNames.java
@@ -0,0 +1,11 @@
+public class BadNames {
+	public static void main(String[] args) {
+		String firstName = "Samira";
+    String name = "Smith";
+    String emailAdress = "samira@google.com";
+    int salaryExpectation = 100000;
+    int yearOfBirth = 1955;
+
+    System.out.println("The program runs!");
+	}
+}
