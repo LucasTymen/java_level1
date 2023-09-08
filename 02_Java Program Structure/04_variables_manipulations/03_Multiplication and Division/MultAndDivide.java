f6090534878406aa910cdd81c04082eff4b07c2d
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-08 12:58:13 +0200

    multiples and divide

---
+++ 02_Java Program Structure/04_variables_manipulations/03_Multiplication and Division/MultAndDivide.java
@@ -0,0 +1,11 @@
+public class MultAndDivide {
+	public static void main(String[] args) {
+		double subtotal = 30;
+    double tax = 0.0875;
+    double total = subtotal + (subtotal * tax);
+    System.out.print("total : ");
+    System.out.println(total);
+    System.out.print("divided by 4 : ");
+    System.out.println(total/4);
+	}
+}
