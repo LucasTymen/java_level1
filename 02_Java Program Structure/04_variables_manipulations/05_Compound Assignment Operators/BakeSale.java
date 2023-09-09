a7a7d8d8600e5fb58cf6252e61b2f4063651c5cf
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-09 10:18:31 +0200

    Compound Assignment Operators

---
+++ 02_Java Program Structure/04_variables_manipulations/05_Compound Assignment Operators/BakeSale.java
@@ -0,0 +1,10 @@
+public class BakeSale {
+	public static void main(String[] args) {
+		int numCookies = 13;
+
+    numCookies -= 3;
+    numCookies /= 2;
+    // Add your code above
+    System.out.println(numCookies);
+	}
+}
