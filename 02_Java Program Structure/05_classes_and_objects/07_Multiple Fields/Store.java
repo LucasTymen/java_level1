94cc1fec29105fc56eb3786b2380c66597234a04
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-11 18:23:29 +0200

    classes_Multiple Fields

--- 02_Java Program Structure/05_classes_and_objects/07_Multiple Fields/Store.java
+++ 02_Java Program Structure/05_classes_and_objects/07_Multiple Fields/Store.java
@@ -1,14 +1,19 @@
 public class Store {
   // instance fields
   String productType;
+  int inventoryCount;
+  double inventoryPrice;
 
   // constructor method
-  public Store(String product) {
+  public Store(String product, int count, double price) {
     productType = product;
+    inventoryCount = count;
+    inventoryPrice = price;
   }
 
   // main method
   public static void main(String[] args) {
-
+    Store LemonHead = new Store("Pulco", 3, 2.3);
+    Store cookieShop = new Store("cookies", 12, 3.75);
   }
 }
