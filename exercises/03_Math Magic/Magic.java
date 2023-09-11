28cd5bded47b79f4bc3f19e38fcaa2a522141dae
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-11 09:13:17 +0200

    exercice3_magic

--- exercises/03_Math Magic/Magic.java
+++ exercises/03_Math Magic/Magic.java
@@ -1,5 +1,15 @@
 public class Magic {
 	public static void main(String[] args) {
-    int myNumber = 3;
+    // myNumber is the original number
+    int myNumber = 77;
+    int stepOne = myNumber*myNumber;
+    int stepTwo = stepOne+myNumber;
+    int stepThree = stepTwo/myNumber;
+    int stepFour = stepThree+17;
+    int stepFive = stepFour-myNumber;
+    int stepSix = stepFive/6;
+
+    //testing routine
+    System.out.println(stepSix);
 	}
 }
