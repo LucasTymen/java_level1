5f6983ab7a9775ba3290de37e0b2f0bd2f3cc82e
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-13 12:57:20 +0200

    methods_Reassigning Instance Fields

---
+++ 02_Java Program Structure/06_methods/06_Reassigning Instance Fields/Store.java
@@ -0,0 +1,22 @@
+public class Store {
+  // instance fields
+  String productType;
+  double price;
+
+  // constructor method
+  public Store(String product, double initialPrice) {
+    productType = product;
+    price = initialPrice;
+  }
+
+  // increase price method
+  public void increasePrice(double priceToAdd){
+
+  }
+
+  // main method
+  public static void main(String[] args) {
+    Store lemonadeStand = new Store("Lemonade", 3.75);
+
+  }
+}
