f4e90db665891c47bcd8bba0a6f60dc1433deaf5
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-13 12:43:21 +0200

    methods_Adding Parameters

--- 02_Java Program Structure/06_methods/05_adding parameters/Store.java
+++ 02_Java Program Structure/06_methods/05_adding parameters/Store.java
@@ -13,9 +13,13 @@ public class Store {
 		System.out.println(message);
   }
 
+  public void greetCustomer(String customer){
+    System.out.println("Welcome to the store, " + customer + "!");
+  }
+
   // main method
   public static void main(String[] args) {
     Store lemonadeStand = new Store("Lemonade");
-
+    lemonadeStand.greetCustomer("Roocky");
   }
 }
