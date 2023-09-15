8c762f0360c61493de5d672d8aa6069eb62ff5e3
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-15 10:44:02 +0200

    methods_toString Method

--- 02_Java Program Structure/06_methods/08_toString Method/Theory.java
+++ 02_Java Program Structure/06_methods/08_toString Method/Theory.java
@@ -2,13 +2,18 @@
 Learn Java: Methods
 The toString() Method
 
-When we print out Objects, we often see a String that is not very helpful in determining what the Object represents. In the last lesson, we saw that when we printed our Store objects, we would see output like:
+When we print out Objects, we often see a String that is not very helpful in determining what the Object represents.
+In the last lesson, we saw that when we printed our Store objects, we would see output like:
 
 Store@6bc7c054
 
+==> https://www.codecademy.com/resources/docs/java/output
+
 where Store is the name of the object and 6bc7c054 is its position in memory.
 
-This doesn’t tell us anything about what the Store sells, the price, or the other instance fields we’ve defined. We can add a method to our classes that makes this printout more descriptive.
+This doesn’t tell us anything about what the Store sells, the price, or the other instance fields we’ve defined.
+We can add a method to our classes that makes this printout more descriptive.
+==> https://www.codecademy.com/resources/docs/java/classes
 
 When we define a toString() method for a class, we can return a String that will print when we print the object:
 */
@@ -39,7 +44,9 @@ Instructions
 In the main() method, print the Objects lemonadeStand and cookieShop. Are these printouts helpful in understanding these Objects?
 2.
 
-Create a toString() method for the Store class. The method signature should say that it is public, and that it returns a String. It shouldn’t take in any parameters. For now, have it return the String "Store".
+Create a toString() method for the Store class.
+The method signature should say that it is public, and that it returns a String.
+It shouldn’t take in any parameters. For now, have it return the String "Store".
 3.
 
 "Store" isn’t very helpful! What kind of Store is it?
@@ -50,7 +57,8 @@ Your String should look like:
 
 This store sells productType at a price of price.
 
-where productType and price are the values in those instance fields. For example, if it was a hat store where hats cost 8, the String would say:
+where productType and price are the values in those instance fields.
+For example, if it was a hat store where hats cost 8, the String would say:
 
 This store sells hats at a price of 8.
 
