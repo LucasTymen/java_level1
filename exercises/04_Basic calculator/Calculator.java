c2aa12b2315d05dd801baf734299fc12a9e7d39b
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-18 11:09:17 +0200

    exercize_calculator_step15

--- exercises/04_Basic calculator/Calculator.java
+++ exercises/04_Basic calculator/Calculator.java
@@ -1,7 +1,10 @@
 public class Calculator {
   public Calculator(){
     //
-
+    /*
+    This is an Abacus that is used to slide beads around and do math.
+    It was created by Laura in 2019.
+    */
   }
 
   public int add(int a, int b){
@@ -30,6 +33,10 @@ public class Calculator {
   };
 
   public static void main(String[] args){
-    //
+    // using addition
+    Calculator myCalculator = new Calculator();
+    System.out.println(myCalculator.add(5, 7));
+
+    System.out.println(myCalculator.subtract(45, 11));
   }
 }
