d370dab5c41c9d515ca9ea131f25ccd9cb7f5a82
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-18 10:43:50 +0200

    exercize_calculator_step15

---
+++ exercises/04_Basic calculator/Calculator.java
@@ -0,0 +1,35 @@
+public class Calculator {
+  public Calculator(){
+    //
+
+  }
+
+  public int add(int a, int b){
+    return a+b;
+    // ADDITION
+  };
+
+  public int subtract(int a, int b){
+    return a-b;
+    // SUBTRACTION
+  };
+
+  public int multiply(int a, int b){
+    return a*b;
+    // MULTIPLICATION
+  };
+
+  public int divide(int a, int b){
+    return a/b;
+    // DIVISION
+  };
+
+  public int modulo(int a, int b){
+    return a%b;
+    //MODULO
+  };
+
+  public static void main(String[] args){
+    //
+  }
+}
