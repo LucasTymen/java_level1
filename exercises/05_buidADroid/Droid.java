fddfe71a297e67d5dd665888b3d42ad7c94f0078
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-18 11:44:08 +0200

    exercize_BuildADroid_step5

---
+++ exercises/05_buidADroid/Droid.java
@@ -0,0 +1,15 @@
+public class Droid{
+  //CLASS DECLARATIONS
+  String name;
+  int batteryLevel;
+
+  // CONSTRUCTOR
+  class public Droid(String droidName){
+    name = droidName;
+    batteryLevel = 100;
+  }
+
+  public static void main(String[] args){
+    //
+  }
+}
