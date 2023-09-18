756f0c079dadec02d09baa043ba0054c416859b8
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-18 16:22:07 +0200

    exercize_BuildADroid_step13

--- exercises/05_buidADroid/Droid.java
+++ exercises/05_buidADroid/Droid.java
@@ -11,17 +11,23 @@ public class Droid{
 
   // PERFORM TASK
   public void performTask(String task) {
+    batteryLevel = batteryLevel - 10;
     System.out.println(name + "is performing task: " + task);
   }
+  // ENERGY REPORT
+
 
   // toString METHOD
   public String toString(){
-    return "Hello, I’m the droid : " + name;
+    return( "Hello, I’m the droid : " + name);
   }
 
   public static void main(String[] args){
     //OBJECTS CREATION 1 - Codey
     Droid Codey = new Droid("Codey");
+    Droid Margot = new Droid("margot");
     System.out.println(Codey);
+    Margot.performTask("levitating");
+    Codey.performTask("Practicing death metal shredding !!!");
   }
 }
