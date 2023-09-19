2266b75e52fb10c7c29b1f290e01b2312de2dcf7
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-19 10:51:13 +0200

    Conditionals and ctrl flow_if statement

---
+++ 02_Java Program Structure/07_Conditionals and Control Flow/01_introductionToControlFlow/Theory.java
@@ -0,0 +1,52 @@
+/*
+Conditionals and Control Flow
+Introduction to Control Flow
+
+Imagine we’re writing a program that enrolls students in courses.
+
+    If a student has completed the prerequisites, then they can enroll in a course.
+    Else, they need to take the prerequisite courses.
+
+They can’t take Physics II without finishing Physics I.
+
+We represent this kind of decision-making in our program using conditional or control flow statements.
+Before this point, our code runs line-by-line from the top down, but conditional statements allow us to be selective in which portions will run.
+==> https://www.codecademy.com/resources/docs/java/conditionals?page_ref=catalog
+
+Conditional statements check a boolean condition and run a block of code depending on the condition.
+Curly braces mark the scope of a conditional block similar to a method or class.
+
+Here’s a complete conditional statement:
+*/
+
+if (true) {
+
+  System.out.println("Hello World!");
+
+}
+
+/*
+If the condition is true, then the block is run. So Hello World! is printed.
+
+But suppose the condition is different:
+*/
+
+if (false) {
+
+  System.out.println("Hello World!");
+
+}
+
+/*
+If the condition is false, then the block does not run.
+
+This code is also called if-then statements: “If (condition) is true, then do something”.
+Instructions
+
+Let’s get started!
+
+Click Next to continue.
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+==> https://www.codecademy.com/learn/learn-java/modules/learn-java-conditionals-control-flow-u/cheatsheet
+*/
