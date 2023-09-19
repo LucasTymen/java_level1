d2d47ddd98c48c16aa5c0a7f53a06189a49cfa60
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-19 17:14:52 +0200

    Conditionals and ctrl flow_review

---
+++ 02_Java Program Structure/07_Conditionals and Control Flow/07_ review/Theory.java
@@ -0,0 +1,41 @@
+/*
+Conditionals and Control Flow
+Review
+
+Before this lesson, our code executed from top to bottom, line by line.
+
+Conditional statements add branching paths to our programs.
+We use conditionals to make decisions in the program so that different inputs will produce different results.
+==> https://www.codecademy.com/resources/docs/java/conditionals
+
+Conditionals have the general structure:
+*/
+
+if (condition) {
+    // consequent path
+} else {
+    // alternative path
+}
+
+/*
+ Specific conditional statements have the following behavior:
+
+    if-then:
+        code block runs if condition is true
+    if-then-else:
+        one block runs if conditions is true
+        another block runs if condition is false
+    if-then-else chained:
+        same as if-then but an arbitrary number of conditions
+    switch:
+        switch block runs if condition value matches case value
+==> https://www.codecademy.com/resources/docs/java/switch
+
+Instructions
+
+Our complete Order program is in the text editor but the main() method is empty.
+
+Create different Order instances and see if you can run the code in all the different conditional blocks!
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+*/
