729b692b1ac7808e27b5624188e6922475bee308
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-20 11:19:41 +0200

    Conditional Operators_Combining Conditional Operators

--- 02_Java Program Structure/07_Conditionals and Control Flow/08_ Conditional Operators/05_Combining Conditional Operators/Operators.java
+++ 02_Java Program Structure/07_Conditionals and Control Flow/08_ Conditional Operators/05_Combining Conditional Operators/Operators.java
@@ -4,13 +4,21 @@ public class Operators {
     int b = 3;
 
     boolean ex1 = !(a == 7 && (b >= a || a != a));
-    // System.out.println(ex1);
+    System.out.println(ex1);
+    //==> !(a== 7 &&(true||false))
+    //==> !(false && false)
+    //==> true
 
     boolean ex2 = a == b || !(b > 3);
-    // System.out.println(ex2);
+    System.out.println(ex2);
+    //==> a == b || !(false)
+    //==> false || true
+    //==> true
 
     boolean ex3 = !(b <= a && b != a + b);
-    // System.out.println(ex3);
+    System.out.println(ex3);
+    //==> !(true && true)
+    //==> false
 
   }
 }
