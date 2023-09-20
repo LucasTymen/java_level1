f0a03c3e4613587de4840ac04a57571b2592a04e
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-20 12:18:38 +0200

    exercise_carLoan_step9

--- exercises/06_carLoan/CarLoan.java
+++ exercises/06_carLoan/CarLoan.java
@@ -1,7 +1,20 @@
 public class CarLoan {
+
 	public static void main(String[] args) {
+    int carLoan = 10000;
+    int loanLength = 3;
+    int interestRate = 5;
+    int downPayement = 2000;
 
+    if(loanLength <= 0 || interestRate <= 0) {
+      System.out.println("Error! You must take out a valid car loan.");
+    }
 
+    if(downPayement >= carLoan) {
+      System.out.println("The car can be paid in full.");
+    } else {
+      System.out.println("financial adjustement");
 
+    }
 	}
 }
