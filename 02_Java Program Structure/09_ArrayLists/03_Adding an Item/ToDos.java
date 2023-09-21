f41dd7d01c6636d207de59236b4040198805371d
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-21 13:48:59 +0200

    ArrayLists_Adding an Item

---
+++ 02_Java Program Structure/09_ArrayLists/03_Adding an Item/ToDos.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class ToDos {
+
+  public static void main(String[] args) {
+
+    ArrayList<String> toDoList = new ArrayList<String>();
+    String toDo1 = "Water plants";
+    // Add more to-dos here:
+
+
+    // Add to-dos to toDoList
+
+
+    System.out.println(toDoList);
+
+
+  }
+
+}
