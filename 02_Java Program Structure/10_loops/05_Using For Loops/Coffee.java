a516a0dc80422f5f2edcd93b02301303a6d2b871
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-22 15:23:08 +0200

    loops_Using For Loops

--- 02_Java Program Structure/10_loops/05_Using For Loops/Coffee.java
+++ 02_Java Program Structure/10_loops/05_Using For Loops/Coffee.java
@@ -2,13 +2,8 @@ class Coffee {
 
   public static void main(String[] args) {
 
-    int cupsOfCoffee = 1;
-
-    while (cupsOfCoffee <= 100) {
-
+    for (int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++) {
       System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
-      cupsOfCoffee++;
-
     }
 
   }
