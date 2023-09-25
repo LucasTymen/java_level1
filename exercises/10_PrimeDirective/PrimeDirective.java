703983b617f4b1ab2a51062b53e39e91710998fd
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-25 16:14:51 +0200

    exercise_Prime Directive

--- exercises/10_PrimeDirective/PrimeDirective.java
+++ exercises/10_PrimeDirective/PrimeDirective.java
@@ -35,6 +35,7 @@ class PrimeDirective {
     PrimeDirective pd = new PrimeDirective();
     int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
     System.out.println(pd.isPrime(2));
+    System.out.println(pd.onlyPrimes(numbers));
   }
 
 }
