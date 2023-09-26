6a3f3db5dc602dd693346247e9900c2a14bd3c5e
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-26 11:58:50 +0200

    exercise_DNA Sequencing_step11

--- exercises/11_DNA Sequencing/DNA.java
+++ exercises/11_DNA Sequencing/DNA.java
@@ -9,10 +9,20 @@ public class DNA {
 
     String dna = dna1;
     int length = dna.length();
+
     int start = dna.indexOf("ATG");
     int stop = dna.indexOf("TGA");
+
     System.out.println("start: " + start);
     System.out.println("Stop: " + stop);
+
+    if (start != -1 && stop != -1){
+      System.out.println("Condition 1 and 2 are satisfied.");
+    }
+
+    if (start != -1 && stop != -1 && (stop - start) % 3 == 0) {
+    System.out.println("Condition 1 and 2 and 3 are satisfied.");
+    }
     //  -. .-.   .-. .-.   .
     //    \   \ /   \   \ /
     //   / \   \   / \   \
