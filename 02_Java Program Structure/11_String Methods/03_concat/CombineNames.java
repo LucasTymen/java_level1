43759ae7e6c4530157d07c1d75411961741ab0fd
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-26 09:57:00 +0200

    stings method _concat

--- 02_Java Program Structure/11_String Methods/03_concat/CombineNames.java
+++ 02_Java Program Structure/11_String Methods/03_concat/CombineNames.java
@@ -3,11 +3,11 @@ public class CombineNames {
 	public static void main(String[] args) {
 
     // Add your first names:
-    String firstName = "";
-    String lastName = "";
+    String firstName = "Mad";
+    String lastName = "Max";
 
     // What's the full name?
-
+    System.out.println(firstName.concat(" " + lastName));
 
   }
 
