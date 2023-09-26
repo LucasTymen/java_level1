a6891600853e2c98f92a6156a9b3d5143982868e
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-26 09:17:04 +0200

    stings method _length

--- 02_Java Program Structure/11_String Methods/02_length/TweetCounter.java
+++ 02_Java Program Structure/11_String Methods/02_length/TweetCounter.java
@@ -5,7 +5,7 @@ public class TweetCounter {
     String tweet = "Liz Lemon, ninjas are kind of cool... I just dont know any personally. Get on that.";
 
     // What's the character count?
-
+    System.out.println(tweet.length());
 
   }
 
