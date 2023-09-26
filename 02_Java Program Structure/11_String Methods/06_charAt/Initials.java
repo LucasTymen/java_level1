890e6e273d239056640541ab4ae33842c0a7040a
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-26 11:01:45 +0200

    stings method _charAt

---
+++ 02_Java Program Structure/11_String Methods/06_charAt/Initials.java
@@ -0,0 +1,16 @@
+public class Initials {
+
+	public static void main(String[] args) {
+
+    // Add a first name and a last name:
+    String firstName = "Robin";
+    String lastName = "Hood";
+
+    // What are the initials?
+    System.out.println(firstName.charAt(0));
+    System.out.println(lastName.charAt(0));
+    System.out.println(firstName.charAt(0) + " " + lastName.charAt(0));
+
+  }
+
+}
---
+++ 02_Java Program Structure/11_String Methods/06_charAt/Theory.java
@@ -0,0 +1,49 @@
+/*String Methods
+charAt()
+
+The charAt() method returns the character located at a String‘s specified index.
+==> https://www.codecademy.com/resources/docs/java/strings/charAt?page_ref=catalog
+
+For example:
+
+String str = "qwer";
+
+System.out.println(str.charAt(2));
+
+It would output e because that’s what’s at index 2. (Once again, indices start with 0.)
+==> https://www.codecademy.com/resources/docs/java/output
+
+Suppose we try to return the character located at index 4.
+It would produce an IndexOutOfBoundsException error because index 4 is out of str‘s range:
+
+java.lang.StringIndexOutOfBoundsException: String index out of range: 4
+
+Instructions
+1.
+
+In Initials.java, add your first name and last name.
+
+Don’t forget to put your names in the double quotes.
+2.
+
+Print out the initials of the firstName and lastName.
+
+For example, suppose the code is:
+
+String firstName = "Codey";
+String lastName = "McCoderson";
+
+System.out.println(firstName.charAt(0));
+System.out.println(lastName.charAt(0));
+
+It would print:
+
+C
+M
+
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+==> https://www.codecademy.com/learn/learn-java/modules/learn-java-string-methods/cheatsheet
+
+
+*/
