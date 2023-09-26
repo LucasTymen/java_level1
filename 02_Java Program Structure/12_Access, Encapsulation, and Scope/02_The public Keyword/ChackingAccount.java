e2c48419c7037a8bbdca0222156bedd6e5e163b5
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-26 13:56:31 +0200

    Access, Encapsulation, and Scope_public Keyword

---
+++ 02_Java Program Structure/12_Access, Encapsulation, and Scope/02_The public Keyword/ChackingAccount.java
@@ -0,0 +1,20 @@
+public class CheckingAccount{
+  private String name;
+  private int balance;
+  private String id;
+
+  public CheckingAccount(String inputName, int inputBalance, String inputId){
+    name = inputName;
+    balance = inputBalance;
+    id = inputId;
+  }
+
+  private void addFunds(int fundsToAdd){
+    balance += fundsToAdd;
+  }
+
+  private void getInfo(){
+    System.out.println("This checking account belongs to " + name +". It has " + balance + " dollars in it.");
+  }
+
+}
