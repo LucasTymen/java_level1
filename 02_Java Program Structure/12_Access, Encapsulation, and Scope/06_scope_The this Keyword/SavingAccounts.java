946885ebe6bf2bbc0324fd93602fc3450e0cd1b4
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-26 20:25:26 +0200

    Access, Encapsulation, and scope_The this Keyword

--- 02_Java Program Structure/12_Access, Encapsulation, and Scope/06_scope_The this Keyword/SavingAccounts.java
+++ 02_Java Program Structure/12_Access, Encapsulation, and Scope/06_scope_The this Keyword/SavingAccounts.java
@@ -6,18 +6,23 @@ public class SavingsAccount{
 
   public SavingsAccount(String owner, int balanceDollar){
     // Complete the constructor
-
+    this.owner = owner;
+    this.balanceDollar = balanceDollar;
+    this.balanceEuro = balanceDollar * 0.85;
   }
 
   public void addMoney(int balanceDollar){
     // Complete this method
+    System.out.println("Adding " + balanceDollar + " dollars to the account.");
+    this.balanceDollar += balanceDollar;
+    System.out.println("The new balance is " + this.balanceDollar + " dollars.");
   }
 
   public static void main(String[] args){
     SavingsAccount zeusSavingsAccount = new SavingsAccount("Zeus", 1000);
 
     // Make a call to addMoney() to test your method
-
+    zeusSavingsAccount.addMoney(2000);
   }
 
 }
