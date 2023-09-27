8cd83abd1c3614ff497c8fee452b1d76f2240250
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-27 15:38:10 +0200

    Static Methods _Static Variables

--- 02_Java Program Structure/13_Static Methods of the Math Class/03_Static Variables/ATM.java
+++ 02_Java Program Structure/13_Static Methods of the Math Class/03_Static Variables/ATM.java
@@ -1,6 +1,8 @@
 public class ATM{
 
   // Step 2: Create your static variables here
+  public static int totalMoney = 0;
+  public static int numATMs = 0;
 
   // Instance variables
   public int money;
@@ -17,8 +19,13 @@ public class ATM{
 
   public static void main(String[] args){
     // Step 1: Create your two ATMs here
+    ATM firstATM = new ATM(1000);
+    ATM secondATM = new ATM(500);
 
     // Step 3: Print your static variable in three different ways here
+    System.out.println(ATM.totalMoney);
+    System.out.println(firstATM.totalMoney);
+    System.out.println(secondATM.totalMoney);
   }
 
 }
