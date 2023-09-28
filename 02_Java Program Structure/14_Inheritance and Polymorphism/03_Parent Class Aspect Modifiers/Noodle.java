97f4fc75f90348a59c9acba987b59bb722cd3764
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-28 14:16:12 +0200

    Inheritance and polymorphysm_Parent Class Aspect Modifiers

---
+++ 02_Java Program Structure/14_Inheritance and Polymorphism/03_Parent Class Aspect Modifiers/Noodle.java
@@ -0,0 +1,32 @@
+public class Noodle {
+
+  private double lengthInCentimeters;
+  private double widthInCentimeters;
+  private String shape;
+  private String ingredients;
+  private String texture = "brittle";
+
+  Noodle(double lenInCent, double wthInCent, String shp, String ingr) {
+
+    this.lengthInCentimeters = lenInCent;
+    this.widthInCentimeters = wthInCent;
+    this.shape = shp;
+    this.ingredients = ingr;
+
+  }
+
+  public boolean isTasty() {
+
+    return true;
+
+  }
+
+  public static void main(String[] args) {
+
+    Ramen yasaiRamen = new Ramen();
+    //System.out.println(yasaiRamen.ingredients);
+    System.out.println(yasaiRamen.isTasty());
+
+  }
+
+}
