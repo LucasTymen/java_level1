4c579e35578a2cd8d33ae13aa3ee720dc59f3d0e
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-29 19:48:37 +0200

    debugging_Exception Handling

--- 02_Java Program Structure/15_debugging/04_Exception Handling/Debug.java
+++ 02_Java Program Structure/15_debugging/04_Exception Handling/Debug.java
@@ -2,8 +2,10 @@ public class Debug {
 
   public static void main(String[] args) {
 
-    int width = 0;
-    int length = 40;
+    try {int width = 0;
+    int length = 40;} catch(ArithmeticException  e){
+      System.err.println("Arithmetic Exceptiondetected here !")
+    }
 
     int ratio = length / width;
 
