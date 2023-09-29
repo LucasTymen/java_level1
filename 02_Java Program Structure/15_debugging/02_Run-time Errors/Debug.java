94b1f3952a64ecd8ef43420eb2a5066631040465
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-29 18:53:18 +0200

    debugging_Run-time Errors

---
+++ 02_Java Program Structure/15_debugging/02_Run-time Errors/Debug.java
@@ -0,0 +1,14 @@
+public class Debug {
+
+  public static void main(String[] args) {
+
+    int width = 0; //misspelled - it was supposed to be 20
+    int length = 40;
+
+    int ratio = length / width;
+
+    System.out.println(ratio);
+
+  }
+
+}
