f12674da08737c9e5115da7a531fde5aa42bd884
Lucas Tymen <lucas.tymen@gmail.com>
2023-09-30 12:17:02 +0200

    2D Arrays_Accessing Elements in a 2D Array

--- 02_Java Program Structure/16_2D Arrays/03_Accessing Elements in a 2D Array/Exercise3.java
+++ 02_Java Program Structure/16_2D Arrays/03_Accessing Elements in a 2D Array/Exercise3.java
@@ -8,10 +8,10 @@ public class Exercise3 {
 		};
 
 		// Access the integer at the first row and fourth column of intMatrix and store it in a variable called retrievedInt
-
+    int retrievedInt = intMatrix[0][3];
 
 		// Print 3 times the center value of intMatrix to the console. Make sure to access the correct element!
-
+    System.out.println(intMatrix[1][2] * 3);
 
     }
 }
