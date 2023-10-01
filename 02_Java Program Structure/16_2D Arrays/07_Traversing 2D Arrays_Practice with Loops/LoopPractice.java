bcbcd867aea071f0198a1e3eac6f794a6b09cf80
Lucas Tymen <lucas.tymen@gmail.com>
2023-10-01 16:42:14 +0200

    2D Arrays_Traversing 2D Arrays_Practice with Loops

---
+++ 02_Java Program Structure/16_2D Arrays/07_Traversing 2D Arrays_Practice with Loops/LoopPractice.java
@@ -0,0 +1,18 @@
+public class LoopPractice {
+	public static void main(String[] args) {
+		String[][] wordData = {{"study", "consider", "examine", "learn"}, {"ponder", "read", "think", "cogitate"}};
+
+		//Use nested enhanced for loops to calculate the total number of characters in the wordData 2D array and print the result to the console. (Get the string .length() of each element)
+		int characterCount = 0;
+
+
+		System.out.println(characterCount);
+
+		//Using nested while loops, iterate through all of the elements in the 2D array and print them to the console using the format: word [row][column]. The formatted print statement has been provided.
+
+
+				//System.out.println(wordData[i][j] + ": [" + i + "]" + "[" + j + "]");
+
+	}
+
+}
